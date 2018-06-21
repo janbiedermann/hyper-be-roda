042ae2544f22823d5adbdac5eb7b87cfad1a4492
Jan Biedermann <jan@kursator.de>
2018-06-21 03:28:44 +0200

    update config version

---
+++ config/hyperloop/hyperloop_webpack_loader.rb
@@ -0,0 +1,13 @@
+require 'opal'
+require 'opal-jquery'
+require 'browser' # CLIENT ONLY
+require 'browser/delay' # CLIENT ONLY
+require 'opal/autoloader'
+require 'opal/autoloader_starter'
+require 'hyper-store'
+require 'hyper-react'
+require 'reactrb/auto-import'
+
+require_tree 'stores'
+require_tree 'models'
+require_tree 'components'
